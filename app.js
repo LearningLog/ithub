cc085b3bd719e228591f773bafa715efa7a08710
yhk <1060553506@qq.com>
2018-03-25 22:42:29 +0800

    完成基本的用户注册功能

--- app.js
+++ app.js
@@ -1,20 +1,32 @@
 // 加载 express 模块
 const express = require('express')
+// 加载 解析表单请求体的第三方包 body-parser
+var bodyParser = require('body-parser')
 // 加载 router 文件模块
 const router = require('./router')
+// *************************************************
 // 实例化 web服务器
 const app = express()
-
+// *************************************************
+// 配置 body-parser
+// 只要加入这个配置，则在 req 请求对象上会多出来一个属性：body
+// 也就是说你就可以直接通过 req.body 来获取表单 POST 请求体数据了
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+// parse application/json
+app.use(bodyParser.json())
+// *************************************************
 // 开放页面所需要的静态资源
 app.use('/public',express.static('./public'))
 app.use('/node_modules', express.static('./node_modules'))
-
+// *************************************************
 // // express-art-template 读取（解析）、渲染、替换
 // express-arttemplate 内部依赖了art-template 
 // 只要经过该配置，那么你就可以使用 res.render 方法 读取、渲染、发送 一次性处理文件了
 app.engine('html', require('express-art-template'))
+// *************************************************
 app.use(router)
-
+// *************************************************
 // 绑定端口号，启动服务器，
 app.listen(3000, () => {
     console.log('running 3000...')
--- controllers/use.js
+++ controllers/use.js
@@ -1,20 +1,85 @@
-exports.showSignin = (req,res) => {
+var mysql = require('mysql');
+const moment = require('moment')
+var connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'root',
+    database: 'ithub'
+});
+// connection.connect();
+
+exports.showSignin = (req, res) => {
     res.render('signin.html')
 }
 
-exports.signin = (req,res) => {
+exports.signin = (req, res) => {
     res.send('signin')
 }
 
-exports.showSignup = (req,res) => {
+exports.showSignup = (req, res) => {
     res.render('signup.html')
 }
-// Username for 'https://github.com':
 
-exports.signup = (req,res) => {
-    res.send('signup')
+
+exports.signup = (req, res) => {
+    // 1. 接收数据
+    // 通过 req.body 来获取表单 POST 请求体数据了
+    const body = req.body
+    // 2. 验证数据，处理数据
+    // 2.1 普通数据校验
+    // 2.1.1 数据是否为空，格式是否正确
+    // 2.2 业务数据校验
+    // 2.2.1 校验邮箱是否被占、校验昵称是否被占
+
+    connection.query(
+        'SELECT * FROM `users` WHERE `email`=?', [body.email],
+        (err, results) => {
+            if (err) {
+                return res.send({
+                    code: 500,
+                    message: err.message // 把错误对象中的错误消息发送给客户端
+                })
+            }
+            if (results[0]) {
+                return res.send({
+                    code: 1,
+                    message: '邮箱已被占用了'
+                })
+            }
+            connection.query('select * from `users` where `nickname`=?', [body.nickname], (err, results) => {
+                if (err) {
+                    return res.send({
+                        code: 500,
+                        message: err.message
+                    })
+                }
+                if (results[0]) {
+                    return res.send({
+                        code: 2,
+                        message: '该昵称已被占用'
+                    })
+                }
+                body.createdAt = moment().format('YYYY-MM-DD HH:mm:ss')
+                connection.query('insert into `users` set ?', body, (err, results) => {
+                    if (err) {
+                        return res.send({
+                            code: 500,
+                            message: err.message
+                        })
+                    }
+                    res.send({
+                        code: 200,
+                        message: 'ok'
+                    })
+                })
+            })
+        });
+
+
+    // 3. 响应数据
+
 }
 
-exports.signout = (req,res) => {
+exports.signout = (req, res) => {
     res.send('signout')
 }
